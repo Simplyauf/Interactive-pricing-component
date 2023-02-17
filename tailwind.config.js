cd013b5bfd6e28890bfa3ac59b4fac814440a01b
ABDULAZEEZ UMARFARUQ <azeezumarfaruk@gmail.com>
2023-02-17 04:08:20 +0100

    fix :forgot to update the build prop due to folder rename

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-	content: ["./public/*.html"],
-	theme: {
-		extend: {
-			colors: {
-				SliderBar: "hsl(174, 77%, 80%)",
-				SliderBg: "hsl(174, 86%, 45%)",
-				MainBg: "hsl(230, 100%, 99%)",
-				CtaBg: "hsl(227, 35%, 25%)",
-				ToggleBg: "hsl(223, 50%, 87%)",
-				Text: "hsl(225, 20%, 60%)",
-				BoxBg: "hsl(0, 0%, 100%)",
-			},
-			screens: {
-				"laptop": "1440px",
-				tablet: { min: "520px", max: "767px" }
-			}
-		},
-	},
-	plugins: [],
+  content: ["./docs/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        SliderBar: "hsl(174, 77%, 80%)",
+        SliderBg: "hsl(174, 86%, 45%)",
+        MainBg: "hsl(230, 100%, 99%)",
+        CtaBg: "hsl(227, 35%, 25%)",
+        ToggleBg: "hsl(223, 50%, 87%)",
+        Text: "hsl(225, 20%, 60%)",
+        BoxBg: "hsl(0, 0%, 100%)",
+      },
+      screens: {
+        laptop: "1440px",
+        tablet: { min: "520px", max: "767px" },
+      },
+    },
+  },
+  plugins: [],
 };
